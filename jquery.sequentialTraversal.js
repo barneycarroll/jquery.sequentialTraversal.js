4775c3f67db2b94d889c816e8cfaa76b97d6a202
Barney Carroll <barney.carroll@gmail.com>
2013-05-16 15:46:42 +0100

    Verbose, legible syntax

--- jquery.sequentialTraversal.js
+++ jquery.sequentialTraversal.js
@@ -31,47 +31,49 @@ $('#span2').sequentialNextAll('span');
 >> ['#span3','#span4']
 */
 
-;(function $sequentialTraversalInit($){
-	var
-		// Define our target methods and abstract their properties
-		methods = {
-			next:    {backwards:0,multiple:0},
-			nextAll: {backwards:0,multiple:1},
-			prev:    {backwards:1,multiple:0},
-			prevAll: {backwards:1,multiple:1}
-		},
-		// New super function to define new functionality.
-		// props object argument defines properties established above,
-		sequentialCrawl = function(props,selector){
-			var
-				// Current element
-				$marker = $(this),
-				// Grab the whole DOM
-				$all    = $('*'),
-				// Eventual return scope
-				$match  = $(),
-				// Whether we've hit the matched element, useful to avoid pointless iteration
-				passed,
-				// Reference for iterated elements
-				$x;
+void function jQuerySequentialTraversalInit($){
+	// Define our target methods and abstract their properties
+	var methods = {
+		next:    {backwards:0,multiple:0},
+		nextAll: {backwards:0,multiple:1},
+		prev:    {backwards:1,multiple:0},
+		prevAll: {backwards:1,multiple:1}
+	};
+
+	// New super function to define new functionality.
+	// props object argument defines properties established above,
+	function sequentialCrawl(props,selector){
+			// Current element
+			var	$marker = $(this);
+			// Grab the whole DOM
+			var $all    = $('*');
+			// Eventual return scope
+			var $match  = $();
+			// Whether we've hit the matched element, useful to avoid pointless iteration
+			var passed;
+			// Reference for iterated elements
+			var $x;
 
 			// Reverse the order if we're to crawl backwards
-			if (props.backwards)
+			if(props.backwards){
 				$all = $($all.get().reverse());
+			}
 
 			// Iterate through flattened DOM
 			$all.each(function sequentialIteration(i){
 				$x = $(this);
 
 				// Do we already have the only match we need?
-				if($match.length && !props.multiple)
+				if($match.length && !props.multiple){
 					return false;
+				}
 
 				// Where are we in relation to the marker?
 				if(!passed){
 					// ...is this it?
-					if ($x.is($marker))
+					if($x.is($marker)){
 						passed = true;
+					}
 					return;
 				};
 
@@ -85,13 +87,11 @@ $('#span2').sequentialNextAll('span');
 		};
 
 	// New bindings
-	$.each(methods,function bindNew(method){
+	$.each(methods, function bindNew(method){
 		$.fn['sequential' + method.charAt(0).toUpperCase() + method.slice(1)] = function sequentialTest(){
-			var $el = $(this);
+			var args = [].unshift.call(arguments, methods[method]);
 
-			arguments = [].unshift.apply('arguments', [methods[method]]);
-
-			return sequentialCrawl.apply($el, arguments);
+			return sequentialCrawl.apply(this, arguments);
 		}
 	});
-}(jQuery));
+}(jQuery);
